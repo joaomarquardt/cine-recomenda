a720da2ecfb3a171920947d2928f8af3450a9d95
joaomarquardt <marquardt1402@gmail.com>
2025-08-29 10:31:58 -0300

    initial commit

---
+++ src/test/java/dev/spring/movie_recommendation/MovieRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spring.movie_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
