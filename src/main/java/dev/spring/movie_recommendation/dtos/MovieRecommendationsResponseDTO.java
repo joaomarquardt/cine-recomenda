7e46bf0c75ff80ecdceb465a2c562ded9688f383
joaomarquardt <marquardt1402@gmail.com>
2025-08-30 17:03:38 -0300

    feat: add attributes in dto to return total results and total pages from external api response

--- src/main/java/dev/spring/movie_recommendation/dtos/MovieRecommendationsResponseDTO.java
+++ src/main/java/dev/spring/movie_recommendation/dtos/MovieRecommendationsResponseDTO.java
@@ -3,6 +3,8 @@ package dev.spring.movie_recommendation.dtos;
 import java.util.List;
 
 public record MovieRecommendationsResponseDTO(
-        List<MovieResponseDTO> results
+        List<MovieResponseDTO> results,
+        Integer total_pages,
+        Integer total_results
 ) {
 }
